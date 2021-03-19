cd4a98f8dae9c97ef9812df883a92e32cad1fc76
Charlotte Pak <charlottepak@outlook.com>
2021-03-19 15:43:11 -0700

    implement losing

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
+
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Game Over");
+    }
+},10);
